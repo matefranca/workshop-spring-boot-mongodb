e71feadcc8bc9bba33bc0b6c944a9dea838cabb0
Matheus Waked Franca <matheus363@hotmail.com>
2023-03-15 16:51:34 -0300

    Consult with several criteria

--- src/main/java/com/matheuswaked/workshopmongo/resources/PostResource.java
+++ src/main/java/com/matheuswaked/workshopmongo/resources/PostResource.java
@@ -1,6 +1,6 @@
 package com.matheuswaked.workshopmongo.resources;
 
-import java.net.URLDecoder;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,5 +34,19 @@ public class PostResource {
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);		
 	}
+	
+	@GetMapping(value="/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
+	
 
 }
